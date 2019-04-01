a78c78c537cbaf75b5a0adad230758d6bc1918f4
akash <akashagarwal37@gmail.com>
2019-04-01 16:48:41 +0530

    initial workstation cookbook commit

--- recipes/hello.rb
+++ recipes/hello.rb
@@ -1,19 +1,11 @@
-package 'vim-enhanced'
-
-package 'nano'
-
-package 'tree' do
-  action:install
-end
-
-package 'git'
-
-package 'ntp'
-
 file '/hello1.txt' do
   content "this is chef test"
   mode '0644'
   owner 'root'
   group 'root'
   action:create
+end
+
+file './test1.txt' do
+  content 'git commit check'
 end
